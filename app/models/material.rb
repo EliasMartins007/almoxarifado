49fbd0f92be10819d672a444db73ba8c46ba4e8c
EliasMartins007 <eliasfaculdade@yahoo.com.br>
2021-09-11 17:07:18 -0300

    ajuste materiais

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,4 +2,5 @@ class Material < ApplicationRecord
 
     validates :nome, presence: true
     validates  :nome, uniqueness: {message: ',já existe um material no sistema com esse nome!'}
+    #validates_length_of :qtd, minimum: 1, message: 'não pode ser negativo!' # não funcionou para negativo
 end
