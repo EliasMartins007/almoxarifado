22b9d3da13b2b88cb6660ee8e55037b1fc280bdc
EliasMartins007 <eliasfaculdade@yahoo.com.br>
2021-09-11 23:21:40 -0300

    validação de negativo

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,5 +2,5 @@ class Material < ApplicationRecord
 
     validates :nome, presence: true, length: {maximum: 255}
     validates  :nome, uniqueness: {message: ',já existe um material no sistema com esse nome!'}
-    #validates :qtd, length: {minimum: 0, message: 'não pode ser negativo!'} # não funcionou para negativo
+    validates_numericality_of :qtd, :only_integer => true, :greater_than_or_equal_to => 0#validates :qtd, numericality: { only_integer: true, greater_than_or_equal_to: 0 }#validates :qtd, length: {minimum: 0, message: 'não pode ser negativo!'} # não funcionou para negativo
 end
